4ca0a3d5e3dc773c9e275182cfda5fffd50f024c
zozo <zozombi@volny.cz>
2020-01-19 19:49:37 +0100

    changed light to moisture

---
+++ enums.d.ts
@@ -0,0 +1,5 @@
+// Auto-generated. Do not edit.
+declare namespace gatorMoisture {
+}
+
+// Auto-generated. Do not edit. Really.
---
+++ gatormoisture.ts
@@ -0,0 +1,55 @@
+/**
+* Andy England @ SparkFun Electronics
+* September 6, 2018
+* https://github.com/sparkfun/pxt-light-bit
+
+* Development environment specifics:
+* Written in Microsoft Makecode
+* Tested with a SparkFun gatorlight sensor and micro:bit
+*
+* This code is released under the [MIT License](http://opensource.org/licenses/MIT).
+* Please review the LICENSE.md file included with this example. If you have any questions
+* or concerns with licensing, please contact techsupport@sparkfun.com.
+* Distributed as-is; no warranty is given.
+*/
+
+
+/**
+ * Functions to operate the gatorlight sensor
+ */
+
+ enum gatorMoistureType{
+	 moisture=1,
+	 adcVal=2,
+ }
+
+
+
+//% color=#f44242 icon="\uf185"
+namespace gatorMoisture {
+
+    // Functions for reading light from the gatorlight in lux or straight adv value
+
+    /**
+    * Reads the number
+    */
+    //% weight=30 blockId="gatorMoisture_moisture" block="Get moisture on pin %pin | in %gatorMoistureType"
+    export function moisture(pin: AnalogPin, type: gatorMoistureType): number{
+      let ADCVal = pins.analogReadPin(pin)
+      switch(type){
+        case gatorMoistureType.moisture: return getMoisture(ADCVal)
+        case gatorMoistureType.adcVal: return ADCVal
+        default: return -11111111
+      }
+    }
+
+	/**
+     * Function used for simulator, actual implementation is in gatorlight.cpp
+     */
+    //% shim=gatorMoisture::getMoisture
+    function getMoisture(ADCVal: number) {
+        // Fake function for simulator
+        return 0
+    }
+
+    }
